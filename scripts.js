e4b79dc985134a0e212a82cc0dbafa1d26118e9a
Cam <37002723+CallMeCam@users.noreply.github.com>
2023-08-19 22:59:27 -0400

    Created grid scripts
    added script for creating static div grid on page. also added hover effect to change background-color on hover

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// create grid of divs
+const gridContainer = document.getElementById('grid-container');
+
+function makeRows(rows, cols) {
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        // cell.innerText = (c + 1);
+        gridContainer.appendChild(cell).className = 'grid-item';
+    };
+};
+
+makeRows(16, 16);
+
+// apply hover effect to grid cells
+const gridCells = document.getElementsByClassName('grid-item');
+const gridCellsArray = [...gridCells];
+
+gridCellsArray.forEach(cell => cell.addEventListener('mouseenter', function () {
+    cell.style.backgroundColor = "pink";
+    cell.style.transition = "background-color 0.3s";
+}));
